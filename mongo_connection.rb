3a732801d9f667b001ffb3162aadd32f64a4bbe1
Aniket Patel <ianiket18@gmail.com>
2017-01-30 11:36:38 -0500

    Global mongo location

--- mongo_connection.rb
+++ mongo_connection.rb
@@ -1,9 +1,10 @@
 require 'mongo'
+@mongo_link = 'mongodb://172.17.0.2:27017/'
 
 module MongoConnection
   Mongo::Logger.logger.level = ::Logger::FATAL
   def mongo_insert_doc(database_name, collection_name, documents)
-    client = Mongo::Client.new('mongodb://172.17.0.2:27017/' + database_name)
+    client = Mongo::Client.new(@mongo_link + database_name)
     collection = client[collection_name]
     if documents.kind_of?(Array)
       collection.insert_many(documents)
@@ -13,7 +14,7 @@ module MongoConnection
   end
 
   def find_document(database_name, collection_name, query)
-    client = Mongo::Client.new('mongodb://172.17.0.2:27017/' + database_name)
+    client = Mongo::Client.new(@mongo_link + database_name)
     collection = client[collection_name]
     result = collection.find( query ).first
   end
