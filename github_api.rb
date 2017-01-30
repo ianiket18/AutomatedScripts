1ada673d380cf6bc72cd981eccba326320644545
Aniket Patel <ianiket18@gmail.com>
2017-01-30 11:29:18 -0500

    Making variables global

--- github_api.rb
+++ github_api.rb
@@ -2,9 +2,12 @@ require 'http'
 require 'json'
 
 @github_url = 'https://api.github.com/'
+@userName = 'ianiket18'
+@playbook_name = 'provision.yml'
 
 module GithubConnection
-  def get_repo_link(repo_name, userName = 'ianiket18')
+  def get_repo_link(repo_name, userName = nil)
+    userName ||= @userName
     response = HTTP.get(@github_url + 'repos/' + userName +'/' + repo_name)
     if response.status.code == 404
       false
@@ -13,13 +16,15 @@ module GithubConnection
     end
   end
 
-  def get_playbook(repo_name, userName, playbook_name = 'provision.yml')
+  def get_playbook_link(repo_name, userName = nil, playbook_name = nil)
+    userName ||= @userName
+    playbook_name ||= @playbook_name
+    puts playbook_name
     response = HTTP.get(@github_url + 'repos/' + userName + '/' + repo_name + '/contents/' + playbook_name)
     if response.status.code == 404
       false
     else
       raw_link = JSON.parse(response.body.to_s)['download_url']
-      playbook = HTTP.get(raw_link).body.to_s
     end
   end
 end
